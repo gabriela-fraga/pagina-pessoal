55619eaf67485f91a27655f86879df5bea946a36
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-04-07 13:51:53 -0300

    adicionado o componente de menu lateral

---
+++ src/components/menu-lateral/menu-lateral.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MenuLateral extends React.Component {
+    render() {
+        return (
+            <div>
+                Menu Lateral
+            </div>
+        );
+    }
+}
