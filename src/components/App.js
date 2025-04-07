afe224ce7afff87d668ad4c29ab70c16c54c74a9
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-04-07 13:51:13 -0300

    movi os arquivos do componente principal app.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import MenuLateral from './menu-lateral/menu-lateral';
+
+function App() {
+  return (
+    <div class="d-flex flex-row">
+      <MenuLateral></MenuLateral>
+      Conteúdo
+    </div>
+  );
+}
+
+export default App;
