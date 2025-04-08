b01b0e268d8b97b61e9d591df6b419e4771108b5
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-04-08 15:08:49 -0300

    corrigido index para ts

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,11 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) throw new Error("Elemento #root não encontrado!");
+
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
