e23b7a6b78533018bfb94188246331c59a820226
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-04-09 14:38:32 -0300

    corrigido arquivo index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
