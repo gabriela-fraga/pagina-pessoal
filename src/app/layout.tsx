646ad55320ef0243e87f35487bf6912b859c6ea1
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-04-23 14:45:49 -0300

    resolução de erros

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import "./index.scss";
 import MenuLateral from "./components/menu-lateral/menu-lateral";
 
-
 export const metadata: Metadata = {
   title: "Gabriela Fraga",
   description: "Página de currículo profissional",
@@ -16,13 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="row min-vh-100 flex-md-row flex-column">
-      <div className="col-12 col-md-3 h-md-100">
-        <MenuLateral></MenuLateral>
-      </div>
-      <div className="col-12 col-md-7 h-md-100" id="root">
-        {children}
-      </div>
-    </div>
+    <html suppressHydrationWarning>
+      <body>
+        <div className="row min-vh-100 flex-md-row flex-column">
+          <div className="col-12 col-md-3 h-md-100">
+            <MenuLateral></MenuLateral>
+          </div>
+          <div className="col-12 col-md-7 h-md-100" id="root">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
