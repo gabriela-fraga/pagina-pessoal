87a04d96946f494c7514c16e5397d7ae879df3d0
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-07-11 14:59:40 -0300

    teste para o metodo clique no github

--- src/app/dados-pessoais/dados-pessoais.test.tsx
+++ src/app/dados-pessoais/dados-pessoais.test.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import DadosPessoais from './page';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import DadosPessoais from "./page";
 
-describe('Dados Pessoais component', () => {
-  it('renders without crashing and displays content', () => {
+const openMock = jest.fn();
+
+beforeAll(() => {
+  global.window.open = openMock;
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Dados Pessoais component", () => {
+  it("renders without crashing and displays content", () => {
     render(<DadosPessoais />);
 
-    expect(screen.getByText(/Profissional com seis anos de experiência/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/Profissional com seis anos de experiência/i)
+    ).toBeInTheDocument();
+  });
+
+  it("deve abrir o github ao clicar no botão", () => {
+    const { container } = render(<DadosPessoais />);
+    const githubBtn = container.querySelector("#githubBtn");
+    fireEvent.click(githubBtn!);
+    expect(openMock).toHaveBeenCalledWith(
+      "http://github.com/gabriela-fraga/",
+      "_blank"
+    );
   });
 });
