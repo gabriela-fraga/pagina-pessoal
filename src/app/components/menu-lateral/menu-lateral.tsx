31bc15f9db84e86d11f18b1baa8d167f2b0aee73
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-07-11 09:42:42 -0300

    botoes de contato e metodos de ação

--- src/app/components/menu-lateral/menu-lateral.tsx
+++ src/app/components/menu-lateral/menu-lateral.tsx
@@ -7,6 +7,21 @@ import Link from "next/link";
 export default function MenuLateral() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const goToLinkedin = () => {
+    window.open(
+      "https://www.linkedin.com/in/gabriela-fraga-medeiros/",
+      "_blank"
+    );
+  };
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText("gabyfragamoreiramedeiros@gmail.com");
+  };
+
+  const goToWhatsapp = () => {
+    window.open("https://wa.me/5531998568440", "_blank");
+  };
+
   return (
     <div className="bg-teal-950 text-white w-full flex flex-col md:h-full">
       {/* Botão para expandir/retrair */}
@@ -18,7 +33,11 @@ export default function MenuLateral() {
       </button>
 
       {/* Conteúdo do menu */}
-      <div className={`${isOpen ? "block" : "hidden"} md:flex md:flex-col md:items-center`}>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:flex md:flex-col md:items-center`}
+      >
         {/* Foto */}
         <div className="w-full flex justify-center my-5">
           <div className="w-36 h-36 rounded-full overflow-hidden">
@@ -32,22 +51,49 @@ export default function MenuLateral() {
 
         {/* Info e links */}
         <div className="flex flex-col items-center w-full">
-          <h1 className="text-center text-xl mb-4 px-4">
+          <h3 className="text-center text-xl mb-4 px-4">
             Gabriela Fraga Moreira Medeiros
-          </h1>
+          </h3>
 
-          <Link className="my-2 hover:underline" href="/dados-pessoais">
+          <Link
+            className="my-2 text-white text-decoration-none"
+            href="/dados-pessoais"
+          >
             Dados Pessoais
           </Link>
-          <Link className="my-2 hover:underline" href="/experiencias">
+          <Link
+            className="my-2 text-white text-decoration-none"
+            href="/experiencias"
+          >
             Experiência
           </Link>
-          <Link className="my-2 hover:underline" href="/educacao">
+          <Link
+            className="my-2 text-white text-decoration-none"
+            href="/educacao"
+          >
             Educação
           </Link>
-          <Link className="my-2 hover:underline" href="/competencias">
+          <Link
+            className="my-2 text-white text-decoration-none"
+            href="/competencias"
+          >
             Competências
           </Link>
+
+          <div className="mt-3 fs-4 flex flex-row">
+            <i
+              className="mr-3 bi bi-whatsapp text-white cursor-pointer"
+              onClick={goToWhatsapp}
+            ></i>
+            <i
+              className="mr-3 bi bi-envelope text-white cursor-pointer"
+              onClick={copyEmail}
+            ></i>
+            <i
+              className="mr-3 bi bi-linkedin text-white cursor-pointer"
+              onClick={goToLinkedin}
+            ></i>
+          </div>
         </div>
       </div>
     </div>
