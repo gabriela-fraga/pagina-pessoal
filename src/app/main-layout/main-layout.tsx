673ce7a7f89cdfa4ce5d74aa1433915782e1038b
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-07-11 14:38:35 -0300

    formatação do texto

--- src/app/main-layout/main-layout.tsx
+++ src/app/main-layout/main-layout.tsx
@@ -6,8 +6,8 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col md:flex-row" >
-      <div className="w-full md:w-1/4 bg-teal-950 text-white flex flex-col">
+    <div className="flex flex-col md:flex-row h-100" >
+      <div className="w-full md:w-1/4 bg-white text-white flex flex-col">
         <MenuLateral />
       </div>
       <div className="w-full md:w-3/4 flex flex-col" id="root">
