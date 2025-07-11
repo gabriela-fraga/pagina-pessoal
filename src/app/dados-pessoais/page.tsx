7bc92859c24e8c20debf6d2d16701fc87281a950
gabriela-fraga <gabyfragamoreiramedeiros@gmail.com>
2025-07-11 09:39:52 -0300

    movendo dados de contato para menu lateral

--- src/app/dados-pessoais/page.tsx
+++ src/app/dados-pessoais/page.tsx
@@ -5,11 +5,6 @@ export default function DadosPessoais() {
     <div className="col-12 p-15">
       <p>Profissional com seis anos de experiência no desenvolvimento de aplicações web utilizando Angular. Possuo inglês fluente, o que me permite colaborar eficazmente com equipes internacionais. Meu objetivo é contribuir para o sucesso de projetos desafiadores e impactar positivamente a experiência dos usuários, com atenção para UX/UI e responsividade dos sistemas, buscando garantir a melhor experiência aos usuários em todos os cenários.</p>
 
-      <p>Me contate!</p>
-      <p>+55 (31) 99856-8440</p>
-      <p>gabyfragamoreiramedeiros@gmail.com</p>
-      <p>https://www.linkedin.com/in/gabriela-fraga-medeiros/</p>
-
       <p>Eu moro aqui ó:</p>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15004.955331731207!2d-43.99119527471683!3d-19.914337952288903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6971e30f2070f%3A0xef577b1ca0d6cbb6!2sPadre%20Eust%C3%A1quio%2C%20Belo%20Horizonte%20-%20MG!5e0!3m2!1spt-BR!2sbr!4v1745948952656!5m2!1spt-BR!2sbr" width="600" height="450" style={{ border: 0 }} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       
