3c6ee40d49b7649e103c2a68558f632cc1204fa6
gmedeiros <gmedeiros@tech-round.com>
2025-11-16 20:33:28 -0300

    estilização dos blocos de experiência

--- src/app/components/experiencia/experiencia.tsx
+++ src/app/components/experiencia/experiencia.tsx
@@ -20,18 +20,27 @@ export default function Experiencia({
     descricao
   }: ExperienciaProps) {
   return(
-    <div className="flex flex-col">
-        <div>{titulo}</div>
-        <div className="flex flex-row">
+    <div className="flex flex-col mb-6">
+        <h5 className="font-['Courier_New',monospace] text-teal-950 font-bold ">{titulo}</h5>
+        <div className="flex flex-row font-['Courier_New',monospace] text-green-700 font-bold mb-1">
             <div>{empresa}</div>
+            &nbsp; | &nbsp;
             <div>{cidade}</div>
-            <div>{inicio}</div> - <div>{fim}</div>
+            &nbsp; | &nbsp;
+            <div>{inicio}</div>
+            &nbsp; - &nbsp;
+            <div>{fim}</div>
         </div>
-        <ul>
+        <div className="flex flex-row">
+          <div className="flex pt-2 pb-3">
+            <div className="flex heigth-100 mx-4 border-1 border-dashed border-r-green-850"></div>
+          </div>
+          <ul className="pl-0 font-['Argesta', monospace]">
             {descricao.map((item:string, index:number) => (
                 <li key={index}>{item}</li>
             ))}
-        </ul>
+          </ul>
+        </div>
     </div>
 
   );
